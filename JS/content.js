d9e31f18a1bac9e18bfe005dde442d2768569aed
Lohitaksh Trehan <lohitakshtrehan@gmail.com>
2017-08-10 21:11:18 +0530

    ui of content for approval
    Signed-off-by: Lohitaksh Trehan <lohitakshtrehan@gmail.com>

---
+++ JS/content.js
@@ -0,0 +1,28 @@
+$(function () {
+    $('.button-collapse').sideNav({
+        menuWidth: 250, // Default is 300
+        edge: 'left', // Choose the horizontal origin
+        closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+        draggable: true, // Choose whether you can drag to open on touch screens,
+        onOpen: function(el) { /* Do Stuff */ }, // A function to be called when sideNav is opened
+      onClose: function(el) { /* Do Stuff */ }, // A function to be called when sideNav is closed//
+    });
+    $('.button2-collapse').sideNav({
+        menuWidth: 250, // Default is 300
+        edge: 'right', // Choose the horizontal origin
+        closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+        draggable: true, // Choose whether you can drag to open on touch screens,
+        onOpen: function(el) { /* Do Stuff */ }, // A function to be called when sideNav is opened
+        onClose: function(el) { /* Do Stuff */ }, // A function to be called when sideNav is closed//
+    });
+    $('.collapsible').collapsible();
+    $('.dropdown-button').dropdown({
+            constrainWidth: false, // Does not change width of dropdown to that of the activator
+            hover: true, // Activate on hover
+            gutter: 3, // Spacing from edge
+            belowOrigin: true, // Displays dropdown below the button
+            alignment: 'right', // Displays dropdown with edge aligned to the left of button
+            stopPropagation: false // Stops event propagation
+        }
+    );
+});
